13b1ad8e00519f4a8de80e00b8217c56ade8f8bd
Juan Miguel Gabarron <juanmi@scrapinghub.com>
2017-03-14 09:44:11 +0100

    Viewset to see the models through the api

---
+++ main_api/viewsets.py
@@ -0,0 +1,8 @@
+from main_page.models import Trade
+from main_api.serializers import TradeSerializer
+from rest_framework.viewsets import ModelViewSet
+
+
+class TradeViewSet(ModelViewSet):
+    queryset = Trade.objects.all().order_by('-date_booked')
+    serializer_class = TradeSerializer
