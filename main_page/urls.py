35a0e3a5b107da117a11f3085d41f06a797ce507
Juan Miguel Gabarron <juanmi@scrapinghub.com>
2017-03-14 09:48:53 +0100

    Configure urls for new_trade html

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from main_page.views import index
+from main_page.views import new_trade
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^newtrade/$', new_trade, name='new_trade'),
 ]
