06633c86c7c90c82f53f35013598dc68031d7456
Juan Miguel Gabarron <juanmi@scrapinghub.com>
2017-03-14 09:49:26 +0100

    Create new view for new_trade html

--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
 
+currencies = [
+    "AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK",
+    "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR",
+    "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN",
+    "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"
+]
+
 # Create your views here.
 def index(request):
     return render(request, 'main_page/index.html')
+
+def new_trade(request):
+    return render(request,
+        'main_page/new_trade.html',
+        {'currencies': currencies}
+    )
