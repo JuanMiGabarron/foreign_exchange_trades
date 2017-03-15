74fb43e76689890fcf40134ae6088bce77854151
Juan Miguel Gabarron <juanmi@scrapinghub.com>
2017-03-15 11:32:34 +0100

    Update models

--- main_page/models.py
+++ main_page/models.py
@@ -1,26 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 from random import choice
 
 
+# we store all the alphanumeric values to avoid a lot of calls throw string
 cases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
 
-#Trade model
+# Trade model
 class Trade(models.Model):
     id = models.CharField(max_length=9, primary_key=True)
     sell_currency = models.CharField(max_length=3)
-    sell_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    sell_amount = models.DecimalField(max_digits=14, decimal_places=2)
     buy_currency = models.CharField(max_length=3)
-    buy_amount = models.DecimalField(max_digits=8, decimal_places=2)
-    rate = models.DecimalField(max_digits=6, decimal_places=5)
+    buy_amount = models.DecimalField(max_digits=14, decimal_places=2)
+    rate = models.DecimalField(max_digits=10, decimal_places=6)
     date_booked = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.id
 
+    # save method its called when a value is created or updated
+    # we know if object is created by checking the id
     def save(self, *args, **kwargs):
         if not self.id:
             while True:
@@ -30,7 +32,6 @@ class Trade(models.Model):
                     super(Trade, self).save(*args, **kwargs)
                     break
 
+    # method to generate the RT******* value
     def pkgen(self, N):
-        return 'TR{}'.format(
-            ''.join(choice(cases) for i in range(N))
-        )
+        return 'TR{}'.format(''.join(choice(cases) for i in range(N)))
